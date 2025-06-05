0f9d3747b2338dc136dec1ad69f53dd7bc6634c8
Gustavo Zanotto <gustavozanotto119@gmail.com>
2025-06-05 20:55:10 -0300

    APS inicio

---
+++ src/App.java
@@ -0,0 +1,24 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Object[][] biblioteca = new Object[5][5];
+
+        //Musica 1
+        biblioteca[0][0] = "Banco";
+        biblioteca[0][1] = "Banco";
+        biblioteca[0][2] = "Banco";
+        biblioteca[0][3] = "Banco";
+        biblioteca[0][4] = "Banco";
+
+        
+    }
+
+
+ 
+}
+/*{{, "Matuê", 1 ,  4.14, false},
+{"V de Vilão", "Matuê", 1, 2.26, false},
+{"Bonjour", "KayBlack", 1, 2.11, false},
+{"V de Vilão", "Matuê", 1, 2.26, false},
+{"V de Vilão", "Matuê", 1, 2.26, false}  
+};*/
